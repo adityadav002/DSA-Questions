79ea84b178331640de209cf43173992c6eaa0b6b
Adityadav002 <aditya.yadav992636@gmail.com>
2025-08-04 15:37:52 +0530

    09_stack

---
+++ 14_Stack/09_Get_Min_At_Pop/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <algorithm> 
+using namespace std;
+
+stack<int> _push(int arr[], int n)
+{
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        if (i == 0)
+            st.push(arr[i]);
+        else
+            st.push(min(arr[i], st.top()));
+    }
+    return st;
+}
+
+void _getMinAtPop(stack<int> s)
+{
+    while (!s.empty()) {
+        cout << s.top() << " "; 
+        s.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 5, 15, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    stack<int> minStack = _push(arr, n);
+
+    cout << "Minimum at each pop: ";
+    _getMinAtPop(minStack);
+
+    return 0;
+}
