f3ba8b50f93d7314f59f6414e21b2e6cdea11313
Adityadav002 <aditya.yadav992636@gmail.com>
2025-08-06 17:37:45 +0530

    05_queue

---
+++ 15_Queue/05_Ticket_Problem/code.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int> q;
+        int n = tickets.size();
+        for (int i = 0; i < n; i++)
+            q.push(i);
+
+        int time = 0;
+
+        while (tickets[k] != 0) {
+            tickets[q.front()]--;
+            if (tickets[q.front()])
+                q.push(q.front());
+
+            q.pop();
+
+            time++;
+        }
+        return time;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> tickets = {2, 3, 2};  
+    int k = 2;                        
+
+    int result = solution.timeRequiredToBuy(tickets, k);
+
+    cout << "Time required to buy tickets: " << result << endl;
+
+    return 0;
+}
